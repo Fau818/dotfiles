9e3abd4a1426a6679678de0e6a347d1f47daf0f2
Fau <Fau818@qq.com>
2024-01-16 21:15:08 +0800

    fix(neovim): add Mason binaries to PATH if has neovim fix(neovim): add yarn as a dependency feat: add `brew.txt` to save utils

--- zsh/.config/zsh/plugins/neovim.zsh
+++ zsh/.config/zsh/plugins/neovim.zsh
@@ -5,6 +5,9 @@ if command -v nvim &> /dev/null; then
   export VIM_CONFIG="$XDG_CONFIG_HOME/nvim"
   export EDITOR='nvim' VISUAL='nvim'
   alias vim=nvim
+
+  # Mason binaries
+  PATH="$XDG_DATA_HOME/nvim/mason/bin:$PATH"
 else
   function __neovim_installer() {
     if command -v brew &> /dev/null; then
@@ -13,14 +16,10 @@ else
       # Install dependencies on Linux
       [ "$(uname)" = 'Linux' ] && sudo apt-get install -y language-pack-en-base kitty xclip gcc g++
       # Install dependencies
-      brew install make ripgrep fd node
+      brew install make ripgrep fd node yarn
       # Install neovim nightly version
       brew install --HEAD neovim
     else echo_red 'Not found: `brew` command'
     fi
   }
 fi
-
-
-# Mason
-[ -d "$XDG_DATA_HOME/nvim/mason/bin" ] && PATH="$XDG_DATA_HOME/nvim/mason/bin:$PATH"
