f5d1c4cebd6a53d7df5aa50245e2a3f9e7e9821e
Fau <Fau818@qq.com>
2024-02-17 17:32:43 +0800

    chore: update brew list chore: update yabai config

--- yabai/.config/yabai/destroy_space_if_no_window.sh
+++ yabai/.config/yabai/destroy_space_if_no_window.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-[ $(yabai -m query --spaces --space | jq '.windows | length') -eq 0 ] && yabai -m space --destroy
+[[ "$(yabai -m query --spaces --space | jq '.windows | length')" == 0 ]] && yabai -m space --destroy
