d0dd5b0a7f378b44506266efb88842fd1896017b
Fau <zhangyuFau@gmail.com>
2024-06-13 15:13:03 +0800

    feat(zsh): update vpn module  * add `__vpn_start` and `__vpn_stop` functions  * add `stopClash` function  * `startClash` run clash with sudo permission for using TUN mode

--- zsh/.config/zsh/plugins/vpn.zsh
+++ zsh/.config/zsh/plugins/vpn.zsh
@@ -4,20 +4,23 @@
 # -----------------------------------
 # -------- VPN Setter
 # -----------------------------------
+function __vpn_start() {
+  export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+  echo_yellow 'VPN is enabled.'
+}
+function __vpn_stop() {
+  unset https_proxy http_proxy all_proxy
+  echo_yellow 'VPN is disabled.'
+}
 function vpn() {
-  if [ "$1" = 'start' ]; then
-    export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-    echo_yellow 'VPN is enabled.'
-    return 0
+  # With parameters
+  if [ "$1" = 'start' ]; then __vpn_start; return 0
+  elif [ "$1" = 'stop' ]; then __vpn_stop; return 0
+  else echo_yellow 'Invalid parameter, use auto toggle.'
   fi
 
-  if [ ! -v https_proxy ]; then
-    export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-    echo_yellow 'VPN is enabled.'
-  else
-    unset https_proxy http_proxy all_proxy
-    echo_yellow 'VPN is disabled.'
-  fi
+  # Auto toggle
+  [ ! -v https_proxy ] && __vpn_start || __vpn_stop
 }
 
 # Quick Start VPN on Linux
@@ -26,10 +29,20 @@ if [ "$(uname)" = 'Linux' ]; then
     local tool_name="$(command -v mihomo &> /dev/null && echo 'mihomo' || echo 'clash')"
     # Check `clash` command
     (! command -v "$tool_name" &> /dev/null) && echo_red 'Not found: `clash/mihomo` command' && return 1
-    # Start clash
-    (pgrep -x "$tool_name" &> /dev/null && echo_yellow 'Clash is already running!') || nohup "$tool_name" > /dev/null &
+    # Start clash (NOTE: if you copied this code, you should use `sudo -E` instead of `sudo` else the config file will be missing.)
+    # NOTE: The `sudo echo -n` is necessray since nohup will be suspended if no sudo permission.
+    (pgrep -x "$tool_name" &> /dev/null && echo_yellow "$tool_name is already running!") || (sudo echo -n && sudo nohup "$(which $tool_name)" > /dev/null &)
     # Start VPN
     vpn start
+  }
+
+  function stopClash() {
+    local tool_name="$(command -v mihomo &> /dev/null && echo 'mihomo' || echo 'clash')"
+    # Check `clash` command
+    (! command -v "$tool_name" &> /dev/null) && echo_red 'Not found: `clash/mihomo` command' && return 1
+    # Find the running clash process and kill it with sudo permission.
+    local tool_pid=$(pgrep -x "$tool_name")
+    ([ -n "$tool_pid" ] && sudo kill "$tool_pid" && echo_yellow "$tool_name has stopped!" && vpn stop) || echo_yellow 'No running process Found!'
   }
 fi
 
