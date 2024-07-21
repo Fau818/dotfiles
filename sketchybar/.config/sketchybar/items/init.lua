6dc477c4c2d7c1248891a93266490c9ed2af90b0
Fau <zhangyuFau@gmail.com>
2024-07-21 18:36:46 +0800

    feat(hammerspoon): add AlDente to special_app_list feat(sketchybar): show `timersMenuBar`

--- sketchybar/.config/sketchybar/items/init.lua
+++ sketchybar/.config/sketchybar/items/init.lua
@@ -27,3 +27,17 @@ require("items.media")
 -- -----------------------------------
 require("items.calendar")
 require("items.widgets")
+
+
+-- NOTE: Show `timersMenuBar`
+sbar.add("item", "Raycast.padding", { position = "right", width = 2 * settings.group_padding })
+local surge = sbar.add("alias", "Raycast", {
+  position = "right",
+  alias = {
+    update_freq = 1,
+    color = colors.stardust,
+  },
+  padding_left  = 0,
+  padding_right = -2 * settings.group_padding,
+  background = { color = colors.transparent }
+})
