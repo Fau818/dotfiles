6879989642ea2952b53e7ec40151bd33e403c0b5
Fau <zhangyuFau@gmail.com>
2024-08-27 17:06:06 -0400

    feat(kitty): update config for 0.26 version style(yazi): use `space` as indentation

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -21,17 +21,17 @@ end
 -- ========== Show User/Group of Files
 -- =============================================
 Status:children_add(function()
-	local h = cx.active.current.hovered
-	if h == nil or ya.target_family() ~= "unix" then
-		return ui.Line {}
-	end
+  local h = cx.active.current.hovered
+  if h == nil or ya.target_family() ~= "unix" then
+    return ui.Line {}
+  end
 
-	return ui.Line {
-		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-		ui.Span(":"),
-		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-		ui.Span(" "),
-	}
+  return ui.Line {
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+    ui.Span(":"),
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+    ui.Span(" "),
+  }
 end, 500, Status.RIGHT)
 
 
@@ -40,8 +40,8 @@ end, 500, Status.RIGHT)
 -- ========== Show Username and Hostname
 -- =============================================
 Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ui.Line {}
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+  if ya.target_family() ~= "unix" then
+    return ui.Line {}
+  end
+  return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end, 500, Header.LEFT)
