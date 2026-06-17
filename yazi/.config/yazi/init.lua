7f5e001ae0053a4f39482f8ae254148acaa985bf
Fau <zhangyuFau@gmail.com>
2026-06-17 17:36:40 -0400

    fix(yazi): when file has no extension, use file(1)

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,8 +1,8 @@
----@diagnostic disable: undefined-global
-
+---@diagnostic disable-next-line: undefined-field
 require("full-border"):setup({ type = ui.Border.ROUNDED })
 require("smart-enter"):setup({ open_multi = true })
 require("git"):setup()
+require("mime-ext.local"):setup { fallback_file1 = true }
 
 
 -- -- =============================================
@@ -55,14 +55,16 @@ end, 500, Header.LEFT)
 -- =============================================
 function Linemode:size_and_mtime()
   local time = math.floor(self._file.cha.mtime or 0)
+  local label
   if time == 0 then
-    time = ""
+    label = ""
   elseif os.date("%Y", time) == os.date("%Y") then
-    time = os.date("%b %d %H:%M", time)
+    label = os.date("%b %d %H:%M", time)
   else
-    time = os.date("%b %d  %Y", time)
+    label = os.date("%b %d  %Y", time)
   end
 
   local size = self._file:size()
-  return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+  ---@diagnostic disable-next-line: undefined-field
+  return string.format("%s %s", size and ya.readable_size(size) or "-", label)
 end
